61761acbed9fecdc0321027f572d916157b618be
Sungil Saul Lee <jangsoo86@gmail.com>
2019-10-16 18:19:37 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const mongojs = require('mongojs');
+const db = mongojs('customerapp', ['users']);
+
+const app = express();
+
+/*
+const logger = function(req, res, next){
+    console.log('Logging...');
+    next();
+};
+
+app.use(logger);
+*/
+
+// View Engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Set Static Path
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    db.users.find((err, docs) => {
+        console.log(docs);
+        res.render('index', {
+            title: 'Customers',
+            users: docs
+        });
+    });
+});
+
+app.post('/users/add', (req, res) => {
+    const newUser = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email
+    };
+    console.log(newUser);
+    db.users.insert(newUser, (err, result) => {
+        if(err) {
+            console.log(err);
+        }
+        res.redirect('/');
+    });
+
+});
+
+app.listen(3000, () => console.log('Server started on port 3000...'));
